e1bd8dcf94ca9f15ef5c3107f72723bc38a08c4c
IriszHolm <felleholmberg@live.se>
2025-01-29 11:44:31 +0200

    blel

--- app/database.py
+++ app/database.py
@@ -1,10 +1,17 @@
 ## connect to database
 import asyncio
 from prisma import Prisma
+import os
+from dotenv import load_dotenv
+
+# Load .env variables
+load_dotenv()
 
 async def main() -> None:
     db = Prisma()
     await db.connect()
+
+    print("Connected to the database!")
 
     await db.disconnect()
 
