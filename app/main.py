d58a0fa244fca0fc45bf7790f1fbf3fa21116f89
siljands <siljands@arcada.fi>
2025-02-04 21:23:41 +0200

    simple users endpoint for registration

--- app/main.py
+++ app/main.py
@@ -2,8 +2,39 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from middleware import auth
 from prisma import Prisma
+from pydantic import BaseModel
 
 app = FastAPI()
+
+fake_users_db = [{"name": "Anna"}, {"name": "Lisa"}, 
+            {
+                'name': 'caspian',
+                'email': 'katt@example.com',
+                'phone': '1234567899',
+                'dob': '1991-11-11T00:00:00Z',  
+                'purchases': 5,
+                'address': {
+                    "street": "123 Main St",
+                    "zipcode": "12345",
+                    "city": "yayLand",
+                    "country": "Country"
+                },
+                'data': {
+                    "gender": "female",
+                    "height": "180cm",
+                    "weight": "100kg"
+                }
+            }]
+
+class User(BaseModel):
+    name: str
+    email: str 
+    #password
+    phone: str
+    dob: str
+    purchases: int
+    adress: str
+    data: str 
 
 db = Prisma()
 
@@ -26,3 +57,13 @@ def read_root():
 async def get_users():
     users = await db.user.find_many()
     return users
+
+#get all users with hardcoded placeholder data
+''' @app.get("/users")
+async def get_users():
+    return {"Users": fake_users_db} '''
+
+#create a new user (register)
+@app.post("/users")
+def create_user(user: User):
+    return user
