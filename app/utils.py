e840fe35ea4b6263a6df26aa591db311dff55c65
molinell <molinell@arcada.fi>
2025-02-16 21:50:59 +0200

    sub as str

--- app/utils.py
+++ app/utils.py
@@ -11,12 +11,11 @@ ALGORITHM = os.getenv('ALGORITHM')
 TOKEN_EXPIRY = os.getenv('TOKEN_EXPIRY')
 
 def create_jwt(user_data: User):
-    exp = datetime.datetime.now(tz=timezone.utc) + datetime.timedelta(hours=TOKEN_EXPIRY)
+    exp = datetime.datetime.now(tz=timezone.utc) + datetime.timedelta(hours=int(TOKEN_EXPIRY))
     payload = {
-        "sub": user_data.id,
+        "sub": str(user_data.id),
         "name": user_data.name,
         "email": user_data.email,
-        "role": user_data.role,
         "exp": exp
     }
 
