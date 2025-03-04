61584768af52cc061def7a48fd601c17faa8459c
molinell <molinell@arcada.fi>
2025-03-04 20:48:00 +0200

    kommatecken

--- app/middleware.py
+++ app/middleware.py
@@ -36,7 +36,7 @@ origins = ["https://store-frontend-git-cna-25-store-frontend.2.rahtiapp.fi",
            "https://email-service-git-email-service-api.2.rahtiapp.fi/",
            "https://invoicing-service-git-invoicing-service.2.rahtiapp.fi/",
            "https://users-frontend-git-cloud-native-apps-users-frontend.2.rahtiapp.fi/",
-           "https://users-frontend-git-cloud-native-apps-users-frontend.2.rahtiapp.fi"
+           "https://users-frontend-git-cloud-native-apps-users-frontend.2.rahtiapp.fi",
            "https://wishlist-git-wishlist.2.rahtiapp.fi/wishlist",
            "https://newsletter-service-git-newsletter-service.2.rahtiapp.fi/",
            ]
